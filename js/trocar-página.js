9fc68502ff291809b15a9960d688b42e14ac69b5
bompacast <141850216+bompacast@users.noreply.github.com>
2023-08-10 20:21:33 -0300

    modificações css
    adição de mais poggerezas

--- js/trocar-página.js
+++ js/trocar-página.js
@@ -1,7 +1,9 @@
 const list_pages = ["home-page","archives","about","founders"];
 function change_page(target) {
+    const sidebar = document.getElementById('sidebar');
     for(var i in list_pages){
         document.getElementById(list_pages[i]).style.display="none";
     }
     document.getElementById(target).style.display="block";
+    sidebar.style.animation="fade_out 500ms forwards";
 }
