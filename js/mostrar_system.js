83dd80cfcf9efa86a9bcdb558ac32888556038d7
bompacast <141850216+bompacast@users.noreply.github.com>
2023-08-10 20:41:27 -0300

    mais mudanças sutis

--- js/mostrar_system.js
+++ js/mostrar_system.js
@@ -1,8 +1,10 @@
+let solar = document.getElementById('solar-system');
 document.getElementById('bompa-icon').addEventListener("mouseenter",function(){
     console.log("oi")
-    document.getElementById('solar-system').style.animation = "aparecer 200ms linear forwards";
+    solar.style.animation = "aparecer 200ms linear forwards";
+    solar.style.display='block';
 })
 document.getElementById('bompa-icon').addEventListener("mouseleave",function(){
     console.log("tcau")
-     document.getElementById("solar-system").style.animation = "desaparecer 200ms linear forwards";
+     solar.style.animation = "desaparecer 200ms linear forwards";
  })
